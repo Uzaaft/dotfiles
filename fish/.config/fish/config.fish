518c9b5cee7d70ba8d8e6e790311b53ca9663bc9
uzaaft <muaf@nmbu.no>
2022-01-28 14:26:07 +0100

    Added newest fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,36 +1,12 @@
-abbr -a yr 'cal -y'
-abbr -a c cargo
-abbr -a e lvim
-abbr -a vi lvim
-abbr -a pr 'gh pr create -t (git show -s --format=%s HEAD) -b (git show -s --format=%B HEAD | tail -n+3)'
-if command -v exa > /dev/null
-	abbr -a l 'exa'
-	abbr -a ls 'exa'
-	abbr -a ll 'exa -l'
-	abbr -a lll 'exa -la'
-else
-	abbr -a l 'ls'
-	abbr -a ll 'ls -l'
-	abbr -a lll 'ls -la'
-end
-
-if command -v fd > /dev/null
-  abbr -a find "fd"
-else
-  abbr -a find "find"
-end
-
-if command -v rg > /dev/null
-  abbr -a grep "rg"
-else
-  abbr -a grep "grep"
-end
-
 source ~/.config/fish/prompt.fish
+source ~/.config/fish/alias.fish
 source ~/.config/fish/env.fish
 source ~/.config/fish/bangbang.fish
 if command -v pazi >/dev/null
   status --is-interactive; and pazi init fish | source
+end
+if command -v zoxide > /dev/null
+  zoxide init fish | source
 end
 
 function cd
@@ -39,13 +15,15 @@ function cd
       onefetch
     end
 end
-thefuck --alias | source
-set -gx PATH $HOME/lib/miniconda/bin $PATH
 
+
+thefuck --alias | source
+
+set -gx COLORTERM "truecolor"
 
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
