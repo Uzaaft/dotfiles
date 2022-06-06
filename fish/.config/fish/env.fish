5623c7bb724c948979f53af4a27a5e215f26040c
muaf <muaf@nmbu.no>
2022-06-06 11:24:56 +0200

    Make paths use env var

--- fish/.config/fish/env.fish
+++ fish/.config/fish/env.fish
@@ -1,20 +1,20 @@
 set -gx NPM_CONFIG_PREFIX /Users/uzaaft/.npm-global
-set -gx PATH /Users/uzaaft/.cargo/bin $PATH
-set -gx PATH /Users/uzaaft/.local/bin $PATH
-set -gx PATH /Users/uzaaft/.npm-global/bin/ $PATH
-set -gx ANDROID_HOME /Users/uzaaft/Library/Android/Sdk
+set -gx PATH $HOME/.cargo/bin $PATH
+set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH $HOME/.npm-global/bin/ $PATH
+set -gx ANDROID_HOME $HOME/Library/Android/Sdk
 set -gx PATH $ANDROID_HOME/emulator $PATH
 set -gx PATH $ANDROID_HOME/tools $PATH
 set -gx PATH $ANDROID_HOME/tools/bin $PATH
 set -gx PATH $ANDROID_HOME/platform-tools $PATH
-set -gx DYLD_LIBRARY_PATH /Users/uzaaft/Qt/5.14.0/clang_64/lib $DYLD_LIBRARY_PATH
+set -gx DYLD_LIBRARY_PATH $HOME/Qt/5.14.0/clang_64/lib $DYLD_LIBRARY_PATH
 set -gx PATH ~/Qt/5.12.12/clang_64/bin/ $PATH
 set -gx GITSTATUS_LOG_LEVEL INFO
 set -gx TMUX_CONFIG_DIR /Users/uzaaft/.config/tmux
 set -gx TERM "wezterm"
 set -gx SCCACHE_SIZE 50G
-set -gx PATH /Users/uzaaft/go/bin $PATH
-set -gx PATH /Users/uzaaft/dotfiles/jetbrains_ide $PATH
+set -gx PATH $HOME/go/bin $PATH
+set -gx PATH $HOME/dotfiles/jetbrains_ide $PATH
 set -gx GEM_HOME $HOME/.gem
 set -gx PATH  $GEM_HOME/bin $PATH
 set -gx _CONDA_ROOT /opt/homebrew/Caskroom/miniforge/base
