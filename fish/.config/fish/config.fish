ab756ed17cefebdcc257c90edebb11746770ced0
uzaaft <muaf@nmbu.no>
2022-12-04 17:06:04 +0000

    feat: Remove all brew spesific path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,8 +17,4 @@ thefuck --alias | source
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-# eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source 
-# <<< conda initialize <<<
 starship init fish | source
