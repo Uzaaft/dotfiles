b2daddf461eef1a6b0581f598706d47e550c275c
Uzair Aftab <uzaaft@outlook.com>
2023-06-23 10:28:41 +0200

    feat: add upstream stuff from felix for sketchybar

--- sketchybar/.config/sketchybar/plugins/yabai.sh
+++ sketchybar/.config/sketchybar/plugins/yabai.sh
@@ -1,39 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
 window_state() {
-  source "$HOME/.config/sketchybar/colors.sh"
-  source "$HOME/.config/sketchybar/icons.sh"
+  source "$CONFIG_DIR/colors.sh"
+  source "$CONFIG_DIR/icons.sh"
 
   WINDOW=$(yabai -m query --windows --window)
-  CURRENT=$(echo "$WINDOW" | jq '.["stack-index"]')
+  STACK_INDEX=$(echo "$WINDOW" | jq '.["stack-index"]')
 
-  args=()
-  if [[ $CURRENT -gt 0 ]]; then
-    LAST=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
-    args+=(--set $NAME icon=$YABAI_STACK icon.color=$RED label.drawing=on label=$(printf "[%s/%s]" "$CURRENT" "$LAST"))
-    yabai -m config active_window_border_color $RED > /dev/null 2>&1 &
+  COLOR=$BAR_BORDER_COLOR
+  ICON=""
 
-  else 
-    args+=(--set $NAME label.drawing=off)
-    case "$(echo "$WINDOW" | jq '.["is-floating"]')" in
-      "false")
-        if [ "$(echo "$WINDOW" | jq '.["has-fullscreen-zoom"]')" = "true" ]; then
-          args+=(--set $NAME icon=$YABAI_FULLSCREEN_ZOOM icon.color=$GREEN)
-          yabai -m config active_window_border_color $GREEN > /dev/null 2>&1 &
-        elif [ "$(echo "$WINDOW" | jq '.["has-parent-zoom"]')" = "true" ]; then
-          args+=(--set $NAME icon=$YABAI_PARENT_ZOOM icon.color=$BLUE)
-          yabai -m config active_window_border_color $BLUE > /dev/null 2>&1 &
-        else
-          args+=(--set $NAME icon=$YABAI_GRID icon.color=$ORANGE)
-          yabai -m config active_window_border_color $WHITE > /dev/null 2>&1 &
-        fi
-        ;;
-      "true")
-        args+=(--set $NAME icon=$YABAI_FLOAT icon.color=$MAGENTA)
-        yabai -m config active_window_border_color $MAGENTA > /dev/null 2>&1 &
-        ;;
-    esac
+  if [ "$(echo "$WINDOW" | jq '.["is-floating"]')" = "true" ]; then
+    ICON+=$YABAI_FLOAT
+    COLOR=$MAGENTA
+  elif [ "$(echo "$WINDOW" | jq '.["has-fullscreen-zoom"]')" = "true" ]; then
+    ICON+=$YABAI_FULLSCREEN_ZOOM
+    COLOR=$GREEN
+  elif [ "$(echo "$WINDOW" | jq '.["has-parent-zoom"]')" = "true" ]; then
+    ICON+=$YABAI_PARENT_ZOOM
+    COLOR=$BLUE
+  elif [[ $STACK_INDEX -gt 0 ]]; then
+    LAST_STACK_INDEX=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
+    ICON+=$YABAI_STACK
+    LABEL="$(printf "[%s/%s]" "$STACK_INDEX" "$LAST_STACK_INDEX")"
+    COLOR=$RED
   fi
+
+  args=(--animate sin 10 --bar border_color=$COLOR
+                         --set $NAME icon.color=$COLOR)
+
+  [ -z "$LABEL" ] && args+=(label.width=0) \
+                  || args+=(label="$LABEL" label.width=40)
+
+  [ -z "$ICON" ] && args+=(icon.width=0) \
+                 || args+=(icon="$ICON" icon.width=30)
 
   sketchybar -m "${args[@]}"
 }
@@ -41,17 +41,21 @@ window_state() {
 windows_on_spaces () {
   CURRENT_SPACES="$(yabai -m query --displays | jq -r '.[].spaces | @sh')"
 
-  args=()
+  args=(--set spaces_bracket drawing=off
+        --set '/space\..*/' background.drawing=on
+        --animate sin 10)
+
   while read -r line
   do
     for space in $line
     do
       icon_strip=" "
       apps=$(yabai -m query --windows --space $space | jq -r ".[].app")
-      for app in $apps
-      do
-        icon_strip+=" $($HOME/.config/sketchybar/plugins/icon_map.sh "$app")"
-      done
+      if [ "$apps" != "" ]; then
+        while IFS= read -r app; do
+          icon_strip+=" $($CONFIG_DIR/plugins/icon_map.sh "$app")"
+        done <<< "$apps"
+      fi
       args+=(--set space.$space label="$icon_strip" label.drawing=on)
     done
   done <<< "$CURRENT_SPACES"
@@ -61,7 +65,7 @@ windows_on_spaces () {
 
 mouse_clicked() {
   yabai -m window --toggle float
-  update
+  window_state
 }
 
 case "$SENDER" in
