8e6b9de29962c8afd8721a21d4c5e54037473866
Uzair Aftab <muaf@nmbu.no>
2025-01-13 07:38:00 +0100

    feat(nix): Extend config options, cleanup config and add more binaries to nix

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -7,15 +7,14 @@ in {
   # https://mipmip.github.io/home-manager-option-search/
 
   home.stateVersion = "24.11";
-
-  fonts.fontconfig.enable = true;
-
   home.packages = with pkgs; [
     fd
     gnused
     ripgrep
     wget
     gh
+    nixd
+    alejandra
   ];
 
   programs.direnv = {
@@ -23,12 +22,10 @@ in {
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
-
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      set fish_greeting # Disable greeting
-    '';
+  home.file = {
+    ".zshenv" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/repositories/github.com/uzaaft/dotfiles/config/.zshenv";
+    };
   };
 
   xdg.configFile = {
@@ -36,34 +33,11 @@ in {
       source = config.lib.file.mkOutOfStoreSymlink ../config/ghostty;
       recursive = true;
     };
-  };
-
-  xdg.configFile = {
     nvim = {
-      source = config.lib.file.mkOutOfStoreSymlink ../config/nvim;
-      recursive = true;
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/repositories/github.com/uzaaft/dotfiles/config/nvim";
+    };
+    zsh = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/repositories/github.com/uzaaft/dotfiles/config/zsh";
     };
   };
-
-  # programs.neovim = {
-  #   enable = true;
-  #   defaultEditor = true;
-  #   extraLuaConfig = ''
-  #     require('user')
-  #   '';
-  #   extraPackages = [
-  #     # Included for nil_ls
-  #     pkgs.cargo
-  #     # Included to build telescope-fzf-native.nvim
-  #     pkgs.cmake
-  #     # Included for LuaSnip
-  #     # pkgs.luajitPackages.jsregexp
-  #   ];
-  #   withNodeJs = true;
-  #   withPython3 = true;
-  #   withRuby = true;
-  #   vimdiffAlias = true;
-  #   viAlias = true;
-  #   vimAlias = true;
-  # };
 }
