87ff6605370a066a587a8dff6e626a316424671d
Uzair Aftab <muaf@nmbu.no>
2025-01-16 15:18:44 +0100

    chore: remove files

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-defaults write com.apple.desktopservices DSDontWriteNetworkStores true
-
-# Disable reopen windows when logging back in
-# To reverse this, do:
-# sudo rm -f ~/Library/Preferences/ByHost/com.apple.loginwindow.*
-sudo chown root ~/Library/Preferences/ByHost/com.apple.loginwindow.*
-sudo chmod 000 ~/Library/Preferences/ByHost/com.apple.loginwindow.*
