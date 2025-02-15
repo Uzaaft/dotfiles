38074ef05722b367f8f6e9ee079a9ba493bc723c
Uzair Aftab <muaf@nmbu.no>
2025-02-15 14:33:00 +0100

    chore: remove outdated config value

--- machines/macbook.nix
+++ machines/macbook.nix
@@ -6,10 +6,6 @@
   # Set in Sept 2024 as part of the macOS Sequoia release.
   system.stateVersion = 5;
 
-  # We install Nix using a separate installer so we don't want nix-darwin
-  # to manage it for us. This tells nix-darwin to just use whatever is running.
-  nix.useDaemon = true;
-
   nix = {
     # We need to enable flakes
     extraOptions = ''
