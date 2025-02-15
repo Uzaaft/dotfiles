63281d6b40e05364acbe6699ec4311875fe31181
Uzair Aftab <muaf@nmbu.no>
2025-02-15 15:52:51 +0100

    feat: add in hashed password

--- users/uzaaft/nixos.nix
+++ users/uzaaft/nixos.nix
@@ -17,7 +17,7 @@
     home = "/home/uzaaft";
     extraGroups = [];
     shell = pkgs.fish;
-    # hashedPassword = "";
+    hashedPassword = "$y$j9T$.Mxn9oR73.QjxJP4G3Tpl0$Qan8YAnO8.PScXdDR3Y1QdyyKzO3uG/RgL8x1EGk1k9";
     # openssh.authorizedKeys.keys = [
     #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGbTIKIPtrymhvtTvqbU07/e7gyFJqNS4S0xlfrZLOaY mitchellh"
     # ];
