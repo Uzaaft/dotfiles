f2b5d7c1829d66b73d4d9aa11492dd0086ff0e62
Uzair Aftab <muaf@nmbu.no>
2025-02-15 15:55:16 +0100

    feat: add public key

--- users/uzaaft/nixos.nix
+++ users/uzaaft/nixos.nix
@@ -18,8 +18,8 @@
     extraGroups = [];
     shell = pkgs.fish;
     hashedPassword = "$y$j9T$.Mxn9oR73.QjxJP4G3Tpl0$Qan8YAnO8.PScXdDR3Y1QdyyKzO3uG/RgL8x1EGk1k9";
-    # openssh.authorizedKeys.keys = [
-    #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGbTIKIPtrymhvtTvqbU07/e7gyFJqNS4S0xlfrZLOaY mitchellh"
-    # ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKYZX17OSEH3mKJsP4oFuaGtr8F5TF/3/RXOCw2cBgps"
+    ];
   };
 }
