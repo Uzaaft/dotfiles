05c67d77aff2fa3ad093c0e4f82a9f904cfa4eb4
Uzair Aftab <muaf@nmbu.no>
2025-02-16 14:51:54 +0100

    fix: Add user to wheel group, and remove hashedPassword for now

--- users/uzaaft/nixos.nix
+++ users/uzaaft/nixos.nix
@@ -15,9 +15,9 @@
   users.users.uzaaft = {
     isNormalUser = true;
     home = "/home/uzaaft";
-    extraGroups = [];
+    extraGroups = ["wheel"];
     shell = pkgs.fish;
-    hashedPassword = "$y$j9T$.Mxn9oR73.QjxJP4G3Tpl0$Qan8YAnO8.PScXdDR3Y1QdyyKzO3uG/RgL8x1EGk1k9";
+    # hashedPassword = "$y$j9T$.Mxn9oR73.QjxJP4G3Tpl0$Qan8YAnO8.PScXdDR3Y1QdyyKzO3uG/RgL8x1EGk1k9";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKYZX17OSEH3mKJsP4oFuaGtr8F5TF/3/RXOCw2cBgps"
     ];
