c27935408e1ea6856163fce6188620ff2ef3257e
Uzair Aftab <muaf@nmbu.no>
2025-02-23 20:38:47 +0100

    feat: add latte-panda config

---
+++ machines/latte-panda-x86_64.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    ./hardware/latte-panda-x86_64.nix
+  ];
+
+  # Setup qemu so we can run x86_64 binaries
+
+  # Disable the default module and import our override. We have
+  # customizations to make this work on aarch64.
+
+  # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
+  nixpkgs.config.allowUnfree = true;
+  # nixpkgs.config.allowUnsupportedSystem = true;
+}
