aace14e159779e7a0490587b7295f4f20d09a0af
Uzair Aftab <muaf@nmbu.no>
2025-02-27 11:21:27 +0100

    feat: tweak graphics settings

--- machines/hardware/ws-nvda-x86.nix
+++ machines/hardware/ws-nvda-x86.nix
@@ -12,6 +12,10 @@
 
   boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "sr_mod"];
 
+  hardware.graphics = {
+    enable = true;
+  };
+
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
@@ -19,7 +23,13 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "570.86.16"; # use latest 1080-ti drivers
+      sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
+      openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
+      settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
+      usePersistenced = false;
+    };
   };
 
   boot.initrd.kernelModules = [];
