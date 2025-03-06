d2b24dbb2cbe507397f5be4ba012d2feabe0c133
Uzair Aftab <muaf@nmbu.no>
2025-03-06 18:52:25 +0100

    feat: Add determinate nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,6 @@
   description = "Example nix-darwin system flake";
 
   inputs = {
-    # Pin our primary nixpkgs repository. This is the main nixpkgs repository
-    # we'll use for our configurations. Be very careful changing this because
-    # it'll impact your entire system.
-
     # We use the unstable nixpkgs repo for some packages.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -14,6 +10,7 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
 
     darwin = {
       url = "github:LnL7/nix-darwin/nix-darwin-24.11";
