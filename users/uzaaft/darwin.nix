582c3870dd958233d7f4735d732e108b3d46a14b
Uzair Aftab <uzaaft@outlook.com>
2025-03-18 08:02:14 +0100

    feat: more macos spesific packages

--- users/uzaaft/darwin.nix
+++ users/uzaaft/darwin.nix
@@ -5,13 +5,16 @@
 }: {
   homebrew = {
     enable = true;
-	taps = [ "koekeishiya/formulae"];
+    taps = ["koekeishiya/formulae"];
 
+    casks = ["1password" "discord" "vmware-fusion" "microsoft-teams" "ghostty@tip" "linear-linear"];
     brews = [
       "koekeishiya/formulae/yabai"
     ];
   };
   nix.enable = false;
+  security.pam.enableSudoTouchIdAuth = true;
+  system.defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
 
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
