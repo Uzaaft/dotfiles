bc25865d823683fe02c18fb565e5ac287e8776f5
Uzair Aftab <uzaaft@outlook.com>
2025-03-18 22:49:40 +0100

    feat: add darwin packages

--- users/uzaaft/darwin.nix
+++ users/uzaaft/darwin.nix
@@ -7,14 +7,39 @@
     enable = true;
     taps = ["koekeishiya/formulae"];
 
-    casks = ["1password" "discord" "vmware-fusion" "microsoft-teams" "ghostty@tip" "linear-linear"];
+    casks = [
+      "1password"
+      "discord"
+      "vmware-fusion"
+      "BetterDisplay"
+      "alt-tab"
+      "orbstack"
+      "bruno"
+      # Required for Norwegian jobs
+      "microsoft-teams"
+      "ghostty@tip"
+      "linear-linear"
+      # Need this for nrf development
+      "visual-studio-code"
+      "nrf-connect"
+      "segger-jlink"
+      "nordic-nrf-command-line-tools"
+      # Testing this out to see what the hype is about
+      "cursor"
+    ];
     brews = [
       "koekeishiya/formulae/yabai"
     ];
   };
   nix.enable = false;
   security.pam.enableSudoTouchIdAuth = true;
-  system.defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+  system = {
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
+    defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+  };
 
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
