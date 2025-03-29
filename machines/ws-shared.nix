45ea77b4bbf53b1f21b9dc08c53203583e54fa10
Uzair Aftab <uzaaft@outlook.com>
2025-03-29 18:03:51 +0100

    feat: make xrand-auto script into niri-auto

--- machines/ws-shared.nix
+++ machines/ws-shared.nix
@@ -78,8 +78,8 @@
 
     # For hypervisors that support auto-resizing, this script forces it.
     # I've noticed not everyone listens to the udev events so this is a hack.
-    (writeShellScriptBin "xrandr-auto" ''
-      xrandr --output Virtual-1 --auto
+    (writeShellScriptBin "niri-auto" ''
+      niri msg output Virtual-1 mode auto
     '')
   ];
 
