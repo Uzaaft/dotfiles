8d488c4f3586af40361f0958ed2ea86ad6978e83
Uzair Aftab <uzaaft@outlook.com>
2025-03-31 19:35:35 +0200

    feat: try out fish shell again

--- users/uzaaft/nixos.nix
+++ users/uzaaft/nixos.nix
@@ -9,7 +9,7 @@
   environment.localBinInPath = true;
 
   # Since we're using fish as our shell
-  programs.zsh = {
+  programs.fish = {
     enable = true;
   };
 
@@ -17,7 +17,7 @@
     isNormalUser = true;
     home = "/home/uzaaft";
     extraGroups = ["wheel" "docker" "lxd"];
-    shell = pkgs.zsh;
+    shell = pkgs.fish;
     hashedPassword = "$6$5h.pMKRtGXli6Ix0$IW1YG5nUV9K0Z.TuXWU0E6N0maqrcWMf1exw0TgkKKDwFZ9grjBFsE.OiNCYiRW.ibuRgEeEVbAn4jrIYiwDZ0";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKYZX17OSEH3mKJsP4oFuaGtr8F5TF/3/RXOCw2cBgps"
