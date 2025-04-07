b944c73d03c27ac84b190a899df478a07a1e3c68
Uzair Aftab <uzaaft@outlook.com>
2025-04-07 19:10:35 +0200

    fix: fix bug with tuigreet caused by using fish

--- users/uzaaft/nixos.nix
+++ users/uzaaft/nixos.nix
@@ -4,6 +4,9 @@
   ...
 }: {
   # https://github.com/nix-community/home-manager/pull/2408
+  # imports = [
+  #   ./waybar.nix
+  # ];
 
   # Add ~/.local/bin to PATH
   environment.localBinInPath = true;
@@ -12,12 +15,13 @@
   programs.fish = {
     enable = true;
   };
+  programs.zsh.enable = true;
 
   users.users.uzaaft = {
     isNormalUser = true;
     home = "/home/uzaaft";
     extraGroups = ["wheel" "docker" "lxd"];
-    shell = pkgs.fish;
+    shell = pkgs.zsh;
     hashedPassword = "$6$5h.pMKRtGXli6Ix0$IW1YG5nUV9K0Z.TuXWU0E6N0maqrcWMf1exw0TgkKKDwFZ9grjBFsE.OiNCYiRW.ibuRgEeEVbAn4jrIYiwDZ0";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKYZX17OSEH3mKJsP4oFuaGtr8F5TF/3/RXOCw2cBgps"
