6d633b10ae8cffd11ec939226fd12ef17c440e08
Uzair Aftab <uzaaft@outlook.com>
2025-05-16 14:18:11 +0200

    feat: neovim cache

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -26,7 +26,7 @@
       substituters = ["https://mitchellh-nixos-config.cachix.org" "https://nix-community.cachix.org"];
       trusted-public-keys = [
         "mitchellh-nixos-config.cachix.org-1:bjEbXJyLrL1HZZHBbO4QALnI5faYZppzkU4D2s0G8RQ="
-        "https://github.com/nix-community/neovim-nightly-overlay#binary-cache"
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
   };
