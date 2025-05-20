fd8375a2fa7995faa4654882fb22e987ed10eae7
Uzair Aftab <uzaaft@outlook.com>
2025-05-20 15:23:50 +0200

    fix: nuke fish

--- config/fish/env.fish
+++
@@ -1,4 +0,0 @@
-set -U GIT_PATH $HOME/repositories/
-set -U FZF_DEFAULT_COMMAND "fd --type f --hidden --follow"
-set -U FZF_DEFAULT_OPTS "--extended"
-set -U FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
