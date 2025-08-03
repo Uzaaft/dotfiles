dcb2806e9b3fb04ddf7415b048a81d1256b8d3f7
Uzair Aftab <uzaaft@outlook.com>
2025-08-03 19:24:20 +0200

    fix: remove duplicate settings

--- machines/ws-nvda-x86_64.nix
+++ machines/ws-nvda-x86_64.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   imports = [
-    ./hardware/ws-nvda-x86.nix
+    ./hardware/ws-nvda-x86_.nix
     ./ws-shared.nix
   ];
 
@@ -15,18 +15,7 @@
   # customizations to make this work on aarch64.
   systemd.network.enable = true;
   networking.useNetworkd = true;
-  hardware.graphics = {
-    enable = true;
-  };
   services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
-    open = true;
-  };
 
   # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
   nixpkgs.config.allowUnfree = true;
