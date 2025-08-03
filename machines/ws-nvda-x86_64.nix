1c31d92ac6e1874b857aef87c663851b6dd8a67b
Uzair Aftab <uzaaft@outlook.com>
2025-08-03 18:59:31 +0200

    feat(nvidia): add networking and nvidia

--- machines/ws-nvda-x86_64.nix
+++ machines/ws-nvda-x86_64.nix
@@ -13,6 +13,20 @@
 
   # Disable the default module and import our override. We have
   # customizations to make this work on aarch64.
+  systemd.network.enable = true;
+  networking.useNetworkd = true;
+  hardware.graphics = {
+    enable = true;
+  };
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    open = true;
+  };
 
   # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
   nixpkgs.config.allowUnfree = true;
