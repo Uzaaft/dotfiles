092e374e1a61b870f65cffb2efb7206a6a237e62
Uzair Aftab <uzaaft@outlook.com>
2025-09-20 11:01:56 +0200

    feat: move to fish

--- users/uzaaft/darwin.nix
+++ users/uzaaft/darwin.nix
@@ -57,11 +57,13 @@
     };
     defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
   };
+  programs.fish.enable = true;
 
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
   system.primaryUser = "uzaaft";
   users.users.uzaaft = {
     home = "/Users/uzaaft";
+    shell = pkgs.fish;
   };
 }
