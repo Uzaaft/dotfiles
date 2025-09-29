5053b2e2a17c7a99f3af8411657c8ac8714511f9
Uzair Aftab <git@uzaaft.me>
2025-09-29 13:27:16 +0200

    ghostty: try out new shader
    ghostty: try out new shader

--- users/uzaaft/ghostty/cursor.glsl
+++ users/uzaaft/ghostty/cursor.glsl
@@ -1,11 +1,10 @@
-float getSdfRectangle(in vec2 p, in vec2 xy, in vec2 b)
-{
+const float DURATION = 0.35;
+const float TRAIL_MAX_OPACITY = 0.1;
+
+float getSdfRectangle(in vec2 p, in vec2 xy, in vec2 b) {
     vec2 d = abs(p - xy) - b;
     return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
 }
-
-// Based on Inigo Quilez's 2D distance functions article: https://iquilezles.org/articles/distfunctions2d/
-// Potencially optimized by eliminating conditionals and loops to enhance performance and reduce branching
 
 float seg(in vec2 p, in vec2 a, in vec2 b, inout float s, float d) {
     vec2 e = b - a;
@@ -27,12 +26,10 @@ float seg(in vec2 p, in vec2 a, in vec2 b, inout float s, float d) {
 float getSdfParallelogram(in vec2 p, in vec2 v0, in vec2 v1, in vec2 v2, in vec2 v3) {
     float s = 1.0;
     float d = dot(p - v0, p - v0);
-
     d = seg(p, v0, v3, s, d);
     d = seg(p, v1, v0, s, d);
     d = seg(p, v2, v1, s, d);
     d = seg(p, v3, v2, s, d);
-
     return s * sqrt(d);
 }
 
@@ -45,11 +42,8 @@ float antialising(float distance) {
 }
 
 float determineStartVertexFactor(vec2 a, vec2 b) {
-    // Conditions using step
-    float condition1 = step(b.x, a.x) * step(a.y, b.y); // a.x < b.x && a.y > b.y
-    float condition2 = step(a.x, b.x) * step(b.y, a.y); // a.x > b.x && a.y < b.y
-
-    // If neither condition is met, return 1 (else case)
+    float condition1 = step(b.x, a.x) * step(a.y, b.y);
+    float condition2 = step(a.x, b.x) * step(b.y, a.y);
     return 1.0 - max(condition1, condition2);
 }
 
@@ -60,28 +54,20 @@ float ease(float x) {
     return pow(1.0 - x, 3.0);
 }
 
-const float DURATION = 0.4; //IN SECONDS
-
-void mainImage(out vec4 fragColor, in vec2 fragCoord)
-{
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     #if !defined(WEB)
     fragColor = texture(iChannel0, fragCoord.xy / iResolution.xy);
     #endif
-    // Normalization for fragCoord to a space of -1 to 1;
+
     vec2 vu = normalize(fragCoord, 1.);
     vec2 offsetFactor = vec2(-.5, 0.5);
 
-    // Normalization for cursor position and size;
-    // cursor xy has the postion in a space of -1 to 1;
-    // zw has the width and height
     vec4 currentCursor = vec4(normalize(iCurrentCursor.xy, 1.), normalize(iCurrentCursor.zw, 0.));
     vec4 previousCursor = vec4(normalize(iPreviousCursor.xy, 1.), normalize(iPreviousCursor.zw, 0.));
 
-    // When drawing a parellelogram between cursors for the trail i need to determine where to start at the top-left or top-right vertex of the cursor
     float vertexFactor = determineStartVertexFactor(currentCursor.xy, previousCursor.xy);
     float invertedVertexFactor = 1.0 - vertexFactor;
 
-    // Set every vertex of my parellogram
     vec2 v0 = vec2(currentCursor.x + currentCursor.z * vertexFactor, currentCursor.y - currentCursor.w);
     vec2 v1 = vec2(currentCursor.x + currentCursor.z * invertedVertexFactor, currentCursor.y);
     vec2 v2 = vec2(previousCursor.x + currentCursor.z * invertedVertexFactor, previousCursor.y);
@@ -92,22 +78,15 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 
     float progress = clamp((iTime - iTimeCursorChange) / DURATION, 0.0, 1.0);
     float easedProgress = ease(progress);
-    // Distance between cursors determine the total length of the parallelogram;
     vec2 centerCC = getRectangleCenter(currentCursor);
     vec2 centerCP = getRectangleCenter(previousCursor);
     float lineLength = distance(centerCC, centerCP);
 
-    vec4 newColor = vec4(fragColor);
-    // Compute fade factor based on distance along the trail
-    float fadeFactor = 1.0 - smoothstep(lineLength, sdfCurrentCursor, easedProgress * lineLength);
+    float fadeFactor = clamp(1.0 - smoothstep(lineLength, sdfCurrentCursor, easedProgress * lineLength), 0., TRAIL_MAX_OPACITY);
+    vec4 fadedTrailColor = mix(fragColor, iCurrentCursorColor, fadeFactor);
+    vec4 newColor =  mix(fragColor, fadedTrailColor, antialising(sdfTrail));
 
-    // Apply fading effect to trail color
-    vec4 fadedTrailColor = iCurrentCursorColor * fadeFactor;
-
-    // Blend trail with fade effect
-    newColor = mix(newColor, fadedTrailColor, antialising(sdfTrail));
-    // Draw current cursor
-    newColor = mix(newColor, iCurrentCursorColor, antialising(sdfCurrentCursor));
+    newColor = mix(newColor, iCurrentCursorColor , antialising(sdfCurrentCursor));
     newColor = mix(newColor, fragColor, step(sdfCurrentCursor, 0.));
     fragColor = mix(fragColor, newColor, step(sdfCurrentCursor, easedProgress * lineLength));
 }
