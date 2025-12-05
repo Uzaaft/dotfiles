4708c3b1dbc8fc8ceb4afd3acfaf6847aae94929
Uzair Aftab <git@uzaaft.me>
2025-12-05 13:29:55 +0100

    nix: update

--- packages/amp/package.nix
+++ packages/amp/package.nix
@@ -7,14 +7,14 @@
   runCommand,
 }:
 let
-  version = "0.0.1764331286-g29e66f";
+  version = "0.0.1764921674-g2aab2c";
 
   srcWithLock = runCommand "amp-src-with-lock" { } ''
     mkdir -p $out
     tar -xzf ${
       fetchurl {
-        url = "https://registry.npmjs.org/@sourcegraph/amp/-/amp-0.0.1764331286-g29e66f.tgz";
-        hash = "sha256-xlJPTlcllPp7wfLiToBKaHo88ByAzry+bq44UXtV5l8=";
+        url = "https://registry.npmjs.org/@sourcegraph/amp/-/amp-0.0.1764921674-g2aab2c.tgz";
+        hash = "sha256-rwdc2QKMv2+BYletQ2GQFiQY0UglIw7EAvRZt5q+WPw=";
       }
     } -C $out --strip-components=1
     cp ${./package-lock.json} $out/package-lock.json
@@ -28,7 +28,7 @@ buildNpmPackage rec {
 
   npmDeps = fetchNpmDeps {
     inherit src;
-    hash = "sha256-CedVosY3yZ0ekY2GZ7MMFjo2RX+xWPP8Z+039GnL2QM=";
+    hash = "sha256-hBR4BPPA3Rd5od4dwW7MnRkYlWCXyRq6Uzm7dMIXPYI=";
   };
 
   dontNpmBuild = true;
